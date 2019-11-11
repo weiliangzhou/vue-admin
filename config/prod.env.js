b901a066d02d7e675519e13f64ab5e6f3b8431d4
zhouweiliang <zhouweiliang@bitao123.com>
2019-11-11 16:28:05 +0800

    cj-azgf-10001

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-    BASE_API: '"http://10.10.10.226:8083/api/admin"'
+    BASE_API: '"http://test.admin.cjduck.com/api/admin"'
 }
